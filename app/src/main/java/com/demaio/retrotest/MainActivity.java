3ff9742cf208b44b9698f10af3b9e369a301662d
hereiam123 <bdemaio1@gmail.com>
2019-09-01 18:40:18 -0400

    Defining Path in Retrofit interface

--- app/src/main/java/com/demaio/retrotest/MainActivity.java
+++ app/src/main/java/com/demaio/retrotest/MainActivity.java
@@ -15,6 +15,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
         textViewResult = findViewById(R.id.text_view_result);
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://jsonplaceholder.typicode.com").addConverterFactory(GsonConverterFactory.create()).build();
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        //getPosts();
+        getComments();
+    }
+
+    private void getPosts(){
         Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
 
         call.enqueue(new Callback<List<Post>>() {
@@ -49,6 +55,37 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void getComments(){
+        Call<List<Comment>> call = jsonPlaceHolderApi.getComments(3);
+
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                if(!response.isSuccessful()){
+                    textViewResult.setText("Code: "+response.code());
+                    return;
+                }
+
+                List<Comment> comments = response.body();
+
+                for(Comment comment: comments){
+                    String content = "";
+                    content += "ID: "+comment.getId()+ "\n";
+                    content+="Text: "+comment.getText()+"\n\n";
+                    content += "ID: "+comment.getPostId()+ "\n";
+                    content+="Text: "+comment.getEmail()+"\n\n";
+                    content+="Text: "+comment.getName()+"\n\n";
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
                 textViewResult.setText(t.getMessage());
             }
         });
