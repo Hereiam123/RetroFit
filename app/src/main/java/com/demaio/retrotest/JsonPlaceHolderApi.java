af012ff80742fd68233e312fb80d463e3e4d0432
hereiam123 <bdemaio1@gmail.com>
2019-09-02 15:14:32 -0400

    Adding Form URL encoded example

--- app/src/main/java/com/demaio/retrotest/JsonPlaceHolderApi.java
+++ app/src/main/java/com/demaio/retrotest/JsonPlaceHolderApi.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
@@ -39,4 +40,8 @@ public interface JsonPlaceHolderApi {
     Call<Post> createPost(@Field("userId") int userId,
                           @Field("title") String title,
                           @Field("body") String text);
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post> createPost(@FieldMap Map<String,String> fields);
 }
